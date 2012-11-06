9ce9c7483ec1809a408ccc83cc846fad9ddd2c4d
Hannes Vogt <hannes@havogt.de>
2012-11-06 16:39:44 +0000

    GpuXXXPatternParity.hxx: parity runs slowest (in Coulomb for fixed t (which runs slowest))
    git-svn-id: https://ssl.qwws.net/svn/hv_cudalattice/trunk@42 62163262-a238-45f2-8cc9-3f949a0b5531

---
+++ src/lattice/access_pattern/GpuLandauPatternParity.hxx
@@ -0,0 +1,87 @@
+/*
+ * GpuTimeslicePattern.hxx
+ *
+ *  Created on: Apr 13, 2012
+ *      Author: vogt
+ */
+
+#ifndef GPULANDAUPATTERNPARITY_HXX_
+#define GPULANDAUPATTERNPARITY_HXX_
+
+#include "../../util/cuda/cuda_host_device.h"
+#include "../../util/datatype/lattice_typedefs.h"
+//#include "../SiteCoord.hxx"
+#include <assert.h>
+
+template<class Site, lat_dim_t T_Ndim, lat_group_dim_t T_Nc> class GpuLandauPatternParity
+{
+public:
+	static const lat_group_dim_t Nc = T_Nc;
+	static const lat_index_t Ndim = T_Ndim;
+	CUDA_HOST_DEVICE static inline lat_index_t getSiteIndex( Site s );
+	CUDA_HOST_DEVICE static inline lat_array_index_t getLinkIndex( Site s, lat_dim_t mu );
+//	CUDA_HOST_DEVICE static inline lat_array_index_t getIndex( int linkIndex, int i, int j, int c );
+	CUDA_HOST_DEVICE static inline lat_array_index_t getIndex( Site s, lat_dim_t mu, lat_group_dim_t i, lat_group_dim_t j, bool c );
+	CUDA_HOST_DEVICE static inline lat_array_index_t getIndexByUnique( lat_array_index_t uniqueIndex, lat_coord_t size[T_Ndim] );
+};
+
+template<class Site, lat_dim_t T_Ndim, lat_group_dim_t T_Nc> lat_index_t GpuLandauPatternParity<Site, T_Ndim,T_Nc>::getSiteIndex( Site s )
+{
+	return  s.getLatticeIndex();
+}
+
+template<class Site, lat_dim_t T_Ndim, lat_group_dim_t T_Nc> lat_array_index_t GpuLandauPatternParity<Site, T_Ndim,T_Nc>::getLinkIndex( Site s, lat_dim_t mu )
+{
+	int muSize = T_Nc*T_Nc*2*s.getLatticeSize();
+	return s.getLatticeIndex()+mu*muSize;
+}
+
+template<class Site, lat_dim_t T_Ndim, lat_group_dim_t T_Nc> lat_array_index_t GpuLandauPatternParity<Site, T_Ndim,T_Nc>::getIndex( Site s, lat_dim_t mu, lat_group_dim_t i, lat_group_dim_t j, bool c )
+{
+	int parity = s.getLatticeIndex() / (s.getLatticeSize()/2);
+
+//	s.getLatticeIndexTimeslice()%(s.getLatticeSizeTimeslice()/2) + (s.getLatticeSizeTimeslice()/2)*( parity + 2 * (c + 2 * ( j + T_Nc *( i + T_Nc * mu ) ) ) )+s[0]*timesliceSize;
+
+	return s.getLatticeIndex()%(s.getLatticeSize()/2) + (s.getLatticeSize()/2)*( parity + 2 * ( c + 2 * ( j + T_Nc *( i + T_Nc * mu ) ) ) );
+//	return s.getLatticeIndex() + s.getLatticeSize()*( mu + T_Ndim * ( c + 2 * ( j + T_Nc * i ) ) );
+//	return s.getLatticeIndex() + s.getLatticeSize()*( c + 2 * ( j + T_Nc *( mu + T_Ndim * i ) ) );
+//	return s.getLatticeIndex() + s.getLatticeSize()*( mu + T_Ndim * ( i + T_Nc * ( j + T_Nc * c ) ) );
+//	return ( c + 2 * ( j + T_Nc *( i + T_Nc * ( mu + T_Ndim * ( s[3] + Nx * ( s[2] + Nx * ( s[1] + Nx * s[0] ) ) ) ) ) ) );
+}
+
+/**
+ * calculate the pattern index from unique index.
+ */
+template<class Site, lat_dim_t T_Ndim, lat_group_dim_t T_Nc> lat_array_index_t GpuLandauPatternParity<Site, T_Ndim, T_Nc>::getIndexByUnique( lat_array_index_t uniqueIndex, lat_coord_t size[T_Ndim] )
+{
+
+//	uniqueIndex /= site.getLatticeSize();
+//	int mu = uniqueIndex /=
+
+
+	bool c = uniqueIndex % 2;
+	uniqueIndex /= 2;
+	lat_group_dim_t j = uniqueIndex % T_Nc;
+	uniqueIndex /= T_Nc;
+	lat_group_dim_t i = uniqueIndex % T_Nc;
+	uniqueIndex /= T_Nc;
+	lat_dim_t mu = uniqueIndex % T_Ndim;
+	uniqueIndex /= T_Ndim;
+	lat_array_index_t latticeIndex = uniqueIndex;
+
+//	printf( "c %d\n", c );
+//	printf( "j %d\n", j );
+//	printf( "i %d\n", i );
+//	printf( "mu %d\n", mu );
+//	printf( "latind %d\n", latticeIndex );
+
+
+	Site s( size ); // parity must not be split for unique index
+	s.setLatticeIndexFromNonParitySplitOrder( latticeIndex );
+
+	int parity = s.getLatticeIndex() / (s.getLatticeSize()/2);
+	return s.getLatticeIndex()%(s.getLatticeSize()/2) + (s.getLatticeSize()/2)*( parity + 2 * ( c + 2 * ( j + T_Nc *( i + T_Nc * mu ) ) ) );
+
+}
+
+#endif /* GPUTIMESLICEPATTERN_HXX_ */
