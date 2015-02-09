d6d8f1efed37a0505e0ae0fa6d4c147f5c250c4d
Hannes Vogt <hannes@havogt.de>
2015-02-09 12:40:29 +0100

    git version is updated in every build

--- lib/gaugefixing/versioning.cmake
+++ lib/gaugefixing/versioning.cmake
@@ -1,3 +1,4 @@
+
 execute_process( COMMAND git branch
                 RESULT_VARIABLE GIT_EXIT_CODE 
                 OUTPUT_QUIET ERROR_QUIET )
@@ -5,14 +6,14 @@ if( GIT_EXIT_CODE EQUAL 0 )
     set( IS_GIT_DIRECTORY ON )
 endif()
 
-if( ${IS_GIT_DIRECTORY} )
+if( IS_GIT_DIRECTORY )
 execute_process(COMMAND git describe --long --dirty
                 OUTPUT_VARIABLE CULGT_VERSION
                 OUTPUT_STRIP_TRAILING_WHITESPACE)
-execute_process( COMMAND bash -c "echo \"\#define CULGT_VERSION_MACRO \\\"${CULGT_VERSION}\\\"\" > ${CMAKE_SOURCE_DIR}/version" )
-else( ${IS_GIT_DIRECTORY} )
+execute_process( COMMAND bash -c "echo \"\#define CULGT_VERSION_MACRO \\\"${CULGT_VERSION}\\\"\" > ${CMAKE_CURRENT_LIST_DIR}/version" )
+else( IS_GIT_DIRECTORY )
     if( NOT EXISTS ${CMAKE_SOURCE_DIR}/version )
         message( WARNING "No version number available" )
-        execute_process( COMMAND bash -c "echo \"\#define CULGT_VERSION_MACRO \\\"undefined\\\"\" > ${CMAKE_SOURCE_DIR}/version" )
+        execute_process( COMMAND bash -c "echo \"\#define CULGT_VERSION_MACRO \\\"undefined\\\"\" > ${CMAKE_CURRENT_LIST_DIR}/version" )
     endif( NOT EXISTS ${CMAKE_SOURCE_DIR}/version )
-endif( ${IS_GIT_DIRECTORY} )
+endif( IS_GIT_DIRECTORY )
