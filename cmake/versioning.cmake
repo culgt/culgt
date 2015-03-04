ae1a89a36783f23026026ca9ac3476d52a02b5e2
Hannes Vogt <hannes@havogt.de>
2015-03-04 12:32:44 +0100

    git versioning allows to generate version for different projects that are used in one application

--- cmake/versioning.cmake
+++ cmake/versioning.cmake
@@ -1,13 +1,26 @@
-function( makeVersionString myString )
-    set( NEW_VERSION_STRING  "#define CULGT_VERSION_MACRO \"${myString}\"" PARENT_SCOPE )
+function( makeVersionString myString myVariableName )
+    set( NEW_VERSION_STRING  "#define ${myVariableName} \"${myString}\"" PARENT_SCOPE )
 endfunction( makeVersionString myString ) 
 
 function( writeVersionString )
-    execute_process( COMMAND bash -c "echo '${NEW_VERSION_STRING}' > ${PATH_TO_CURRENT_PROJECT}/version" )
+    execute_process( COMMAND bash -c "echo '${NEW_VERSION_STRING}' > ${PATH_TO_CURRENT_PROJECT}/${CULGT_VERSION_FILE}" )
 endfunction( writeVersionString )
+
+if( NOT DEFINED PATH_TO_FOREIGN_GIT_PROJECT )
+    set( PATH_TO_FOREIGN_GIT_PROJECT ${PATH_TO_CURRENT_PROJECT} )
+endif()
+
+if( NOT DEFINED CULGT_VERSION_VARIABLE )
+    set( CULGT_VERSION_VARIABLE "CULGT_VERSION_MACRO" )
+endif()
+
+if( NOT DEFINED CULGT_VERSION_FILE )
+    set( CULGT_VERSION_FILE "version" )
+endif()
 
 # check if we are in a git managed project
 execute_process( COMMAND git branch
+                WORKING_DIRECTORY ${PATH_TO_FOREIGN_GIT_PROJECT}
                 RESULT_VARIABLE GIT_EXIT_CODE 
                 OUTPUT_QUIET ERROR_QUIET )
 if( GIT_EXIT_CODE EQUAL 0 )
@@ -20,6 +33,7 @@ endif()
 if( IS_GIT_DIRECTORY )
     # get version from git
     execute_process(COMMAND git describe --long --dirty
+                    WORKING_DIRECTORY ${PATH_TO_FOREIGN_GIT_PROJECT}
                     OUTPUT_VARIABLE CULGT_VERSION
                     ERROR_VARIABLE DONTPRINTERRORS
                     OUTPUT_STRIP_TRAILING_WHITESPACE
@@ -27,11 +41,13 @@ if( IS_GIT_DIRECTORY )
     
     if( GIT_EXIT_CODE EQUAL 0 )
         # read old version from version file
-        if( EXISTS ${CMAKE_CURRENT_LIST_DIR}/version )
-            execute_process( COMMAND cat "${PATH_TO_CURRENT_PROJECT}/version" OUTPUT_VARIABLE OLD_VERSION_STRING OUTPUT_STRIP_TRAILING_WHITESPACE )
-        endif( EXISTS ${CMAKE_CURRENT_LIST_DIR}/version )              
-                        
-        makeVersionString( ${CULGT_VERSION} )                
+        if( EXISTS ${PATH_TO_CURRENT_PROJECT}/${CULGT_VERSION_FILE} )
+            execute_process( COMMAND cat "${PATH_TO_CURRENT_PROJECT}/${CULGT_VERSION_FILE}"
+                                WORKING_DIRECTORY ${PATH_TO_FOREIGN_GIT_PROJECT}
+                                OUTPUT_VARIABLE OLD_VERSION_STRING OUTPUT_STRIP_TRAILING_WHITESPACE )
+        endif( EXISTS ${PATH_TO_CURRENT_PROJECT}/${CULGT_VERSION_FILE} )              
+                       
+        makeVersionString( ${CULGT_VERSION} ${CULGT_VERSION_VARIABLE} )                
         
         # write new version file only if different from old version (avoids recompile if nothing changed)
         if( NOT OLD_VERSION_STRING STREQUAL NEW_VERSION_STRING )
@@ -40,15 +56,15 @@ if( IS_GIT_DIRECTORY )
     
     else()
         message( "No cuLGT version number available" )
-        makeVersionString( "undefined" )       
+        makeVersionString( "undefined" ${CULGT_VERSION_VARIABLE} )       
         writeVersionString() 
     endif()
     
 else( IS_GIT_DIRECTORY )
     # if we are not git managed, a version file should come with the distribution (if not set version to undefined)
-    if( NOT EXISTS ${CMAKE_CURRENT_LIST_DIR}/version )
+    if( NOT EXISTS ${PATH_TO_CURRENT_PROJECT}/${CULGT_VERSION_FILE} )
         message( "No cuLGT version number available" )
-        makeVersionString( "undefined" )       
+        makeVersionString( "undefined" ${CULGT_VERSION_VARIABLE} )       
         writeVersionString() 
-    endif( NOT EXISTS ${CMAKE_CURRENT_LIST_DIR}/version )
+    endif( NOT EXISTS ${PATH_TO_CURRENT_PROJECT}/${CULGT_VERSION_FILE} )
 endif( IS_GIT_DIRECTORY )
