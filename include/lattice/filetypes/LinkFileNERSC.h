e75ed58ea040e06182942f3234a9ae8f88eaf077
Hannes Vogt <hannes@havogt.de>
2015-07-02 14:54:52 +0200

    implement writing of NERSC files most header entries are written as n/a or set to "0"

--- include/lattice/filetypes/LinkFileNERSC.h
+++ include/lattice/filetypes/LinkFileNERSC.h
@@ -34,13 +34,14 @@ private:
 	typedef typename MemoryConfigurationPattern::PARAMTYPE::REALTYPE REALTYPE;
 	static const lat_dim_t memoryNdim = MemoryConfigurationPattern::SITETYPE::NDIM;
 
-	std::vector<string> headerLines;
 	std::map<string,string> headerData;
 
 	bool bigendian;
 	bool isDouble;
 	int latticeSizeInFile[4];
-	NerscDatatype datatype;
+	NerscDatatype datatype = NERSC_4D_SU3_GAUGE;
+
+	bool fullHeaderIsAvailable = false;
 
 public:
 	typedef LinkFile<MemoryConfigurationPattern> super;
@@ -55,9 +56,8 @@ public:
 
 	virtual void saveImplementation() CULGT_OVERRIDE
 	{
-//		assert(false);
 		saveHeader();
-//		saveBody();
+		saveBody();
 	}
 
 	virtual void loadImplementation() CULGT_OVERRIDE
@@ -67,14 +67,16 @@ public:
 		loadBody();
 	}
 
-	void readHeaderLines()
+	std::vector<string> readHeaderLines()
 	{
+		std::vector<string> headerLines;
 		string line;
 		while( std::getline( super::file, line ) )
 		{
 			headerLines.push_back( line );
 			if( line.compare( "END_HEADER" ) == 0 ) break;
 		}
+		return headerLines;
 	}
 
 	bool isNotBeginOrEnd( string key )
@@ -112,6 +114,24 @@ public:
 		}
 	}
 
+	string getFloatingPointString()
+	{
+		if( getSizeOfReal() == 8 )
+		{
+			if( bigendian )
+				return "IEEE64BIG";
+			else
+				return "IEEE64";
+		}
+		else
+		{
+			if( bigendian )
+				return "IEEE32BIG";
+			else
+				return "IEEE32";
+		}
+	}
+
 	void parseLatticeSize()
 	{
 		istringstream( headerData["DIMENSION_4"] ) >> latticeSizeInFile[0];
@@ -132,7 +152,7 @@ public:
 		else throw LinkFileException( "unknown NERSC datatype" );
 	}
 
-	void parseHeader()
+	void parseHeader( std::vector<string>& headerLines )
 	{
 		for( std::vector<string>::iterator it = headerLines.begin(); it != headerLines.end(); ++it )
 		{
@@ -158,8 +178,10 @@ public:
 
 	void loadHeader()
 	{
-		readHeaderLines();
-		parseHeader();
+		std::vector<string> headerLines = readHeaderLines();
+		parseHeader( headerLines );
+
+		fullHeaderIsAvailable = true;
 
 		for( std::map<string,string>::iterator it = headerData.begin(); it != headerData.end(); ++it )
 		{
@@ -167,18 +189,56 @@ public:
 		}
 	}
 
+	void makeHeaderData()
+	{
+		headerData.clear();
+		headerData["HDR_VERSION"] = "1.0";
+		if( datatype == NERSC_4D_SU3_GAUGE )
+		{
+			headerData["DATATYPE"] = "4D_SU3_GAUGE";
+		}
+		else
+		{
+			headerData["DATATYPE"] = "4D_SU3_GAUGE_3x3";
+		}
+		headerData["DIMENSION_1"] = std::to_string( this->getLatticeDimension().getDimension(1) );
+		headerData["DIMENSION_2"] = std::to_string( this->getLatticeDimension().getDimension(2) );
+		headerData["DIMENSION_3"] = std::to_string( this->getLatticeDimension().getDimension(3) );
+		headerData["DIMENSION_4"] = std::to_string( this->getLatticeDimension().getDimension(0) );
+
+
+		headerData["FLOATING_POINT"] = getFloatingPointString();
+
+		headerData["CHECKSUM"] = "0";
+		headerData["LINK_TRACE"] = "0";
+		headerData["PLAQUETTE"] = "0";
+		headerData["CREATOR"] = "n/a";
+		headerData["CREATOR_HARDWARE"] = "n/a";
+		headerData["ARCHIVE_DATE"] = "Thu Jan 01 00:00:00 1970";
+		headerData["ENSEMBLE_LABEL"] = "n/a";
+		headerData["ENSEMBLE_ID"] = "000";
+		headerData["BETA"] = "0.0";
+	}
+
 	void saveHeader()
 	{
-		for( auto it = headerLines.begin(); it != headerLines.end(); ++it )
+		if( !fullHeaderIsAvailable )
 		{
-			super::file << *it << std::endl;
+			makeHeaderData();
 		}
+
+		super::file << "BEGIN_HEADER" << std::endl;
+		for( std::map<string,string>::iterator it = headerData.begin(); it != headerData.end(); ++it )
+		{
+			super::file << it->first << " = " << it->second << std::endl;
+		}
+		super::file << "END_HEADER" << std::endl;
 	}
 
 	LocalLink<SUNRealFull<MemoryConfigurationPattern::PARAMTYPE::NC,REALTYPE> > getNextLink()
 	{
-		if( datatype == NERSC_4D_SU3_GAUGE ) return getNextLink3x2();
-		else return getNextLink3x3();
+		if( datatype == NERSC_4D_SU3_GAUGE_3x3 ) return getNextLink3x3();
+		else return getNextLink3x2();
 	}
 
 	LocalLink<SUNRealFull<MemoryConfigurationPattern::PARAMTYPE::NC,REALTYPE> > getNextLink3x3()
@@ -229,28 +289,38 @@ public:
 
 	void writeNextLink( LocalLink<SUNRealFull<MemoryConfigurationPattern::PARAMTYPE::NC,REALTYPE> > link )
 	{
-//		typedef SUNRealFull<MemoryConfigurationPattern::PARAMTYPE::NC,REALTYPE> LocalLinkParamType;
-//
-//		for( int i = 0; i < LocalLinkParamType::SIZE; i++ )
-//		{
-//			if( super::reinterpretReal == STANDARD )
-//			{
-//				typename LocalLinkParamType::TYPE value = link.get( i );
-//				LinkFile<MemoryConfigurationPattern>::file.write( (char*)&value, sizeof(typename LocalLinkParamType::TYPE) );
-//			}
-//			else if( super::reinterpretReal == FLOAT )
-//			{
-//				float value = (float)link.get(i);
-//				LinkFile<MemoryConfigurationPattern>::file.write( (char*)&value, sizeof(float) );
-//			}
-//			else if( super::reinterpretReal == DOUBLE )
-//			{
-//				double value = (double)link.get(i);
-//				LinkFile<MemoryConfigurationPattern>::file.write( (char*)&value, sizeof(double) );
-//			}
-//		}
+		if( datatype == NERSC_4D_SU3_GAUGE_3x3 ) writeNextLink3x3( link );
+			else writeNextLink3x2( link );
 	}
 
+	void writeNextLink3x3( LocalLink<SUNRealFull<MemoryConfigurationPattern::PARAMTYPE::NC,REALTYPE> > link )
+	{
+		writeNextLink( link, 3*3*2 );
+	}
+
+	void writeNextLink3x2( LocalLink<SUNRealFull<MemoryConfigurationPattern::PARAMTYPE::NC,REALTYPE> > link )
+	{
+		writeNextLink( link, 3*2*2 );
+	}
+
+	void writeNextLink( LocalLink<SUNRealFull<MemoryConfigurationPattern::PARAMTYPE::NC,REALTYPE> > link, int parametersToWrite )
+	{
+		typedef SUNRealFull<MemoryConfigurationPattern::PARAMTYPE::NC,REALTYPE> LocalLinkParamType;
+
+		for( int i = 0; i < 12; i++ )
+		{
+			if( getSizeOfReal() == 8 )
+			{
+				double value = (double)link.get(i);
+				super::writeDouble( value, bigendian );
+			}
+			else
+			{
+				float value = (float)link.get(i);
+				super::writeFloat( value, bigendian );
+			}
+		}
+	}
 
 	void loadBody()
 	{
@@ -296,30 +366,59 @@ public:
 
 	void saveBody()
 	{
-//		for( int i = 0; i < this->getLatticeDimension().getSize(); i++ )
-//		{
-//			for( int mu = 0; mu < memoryNdim; mu++ )
-//			{
-//				typename MemoryConfigurationPattern::SITETYPE site( this->getLatticeDimension(), NULL );
-//				site.setIndexFromNonParitySplitOrder( i );
-//
-//				GlobalLink<MemoryConfigurationPattern> src( this->getPointerToU(), site, mu );
-//
-//				LocalLink<SUNRealFull<MemoryConfigurationPattern::PARAMTYPE::NC,REALTYPE> > dest;
-//
-//				dest = src;
-//
-//				writeNextLink( dest );
-//			}
-//		}
+		SiteCoord<4,NO_SPLIT> siteFile( super::getLatticeDimension() );
+		for( int t = 0; t < super::getLatticeDimension().getDimension(0); t++ )
+		{
+			siteFile[0] = t;
+			for( int z = 0; z < super::getLatticeDimension().getDimension(3); z++ )
+			{
+				siteFile[3] = z;
+				for( int y = 0; y < super::getLatticeDimension().getDimension(2); y++ )
+				{
+					siteFile[2] = y;
+					for( int x = 0; x < super::getLatticeDimension().getDimension(1); x++ )
+					{
+						siteFile[1] = x;
+						for( int filemu = 0; filemu < memoryNdim; filemu++ )
+						{
+							int mu;
+							if( filemu == 3 )
+							{
+								mu = 0;
+							}
+							else
+							{
+								mu = filemu+1;
+							}
+
+
+							typename MemoryConfigurationPattern::SITETYPE site( this->getLatticeDimension(), NULL );
+							site.setIndexFromNonParitySplitOrder( siteFile.getIndex() );
+
+							GlobalLink<MemoryConfigurationPattern> src( this->getPointerToU(), site, mu );
+
+							LocalLink<SUNRealFull<MemoryConfigurationPattern::PARAMTYPE::NC,REALTYPE> > dest;
+
+							dest = src;
+
+							writeNextLink( dest );
+						}
+					}
+				}
+			}
+		}
+	}
+
+	int getSizeOfReal()
+	{
+		if( super::reinterpretReal == FLOAT ) return sizeof( float );
+		else if( super::reinterpretReal == DOUBLE ) return sizeof( double );
+		else return sizeof( typename MemoryConfigurationPattern::PARAMTYPE::REALTYPE );
 	}
 
 	void verify()
 	{
-		int mySizeOfReal;
-		if( super::reinterpretReal == FLOAT ) mySizeOfReal = sizeof( float );
-		else if( super::reinterpretReal == DOUBLE ) mySizeOfReal = sizeof( double );
-		else mySizeOfReal = sizeof( typename MemoryConfigurationPattern::PARAMTYPE::REALTYPE );
+		int mySizeOfReal = getSizeOfReal();
 
 		int fileSizeOfReal;
 		if( isDouble ) fileSizeOfReal = 8;
@@ -341,11 +440,6 @@ public:
 				super::throwException( msg.str(), this->getLatticeDimension().getDimension(i), latticeSizeInFile[i] );
 			}
 		}
-	}
-
-	void loadData()
-	{
-
 	}
 };
 
